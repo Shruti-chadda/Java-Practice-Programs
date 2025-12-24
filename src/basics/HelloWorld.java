53f620f5702d30e47d74cb5cc048d724a8cafaa5
Shruti-chadda <chaddashrutiwork@gmail.com>
2025-12-24 18:26:21 +0530

    Add Hello World program

---
+++ src/basics/HelloWorld.java
@@ -0,0 +1,11 @@
+package basics;
+
+// Program: Hello World
+// Purpose: Print Hello World in Java
+// Output: Hello World
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
